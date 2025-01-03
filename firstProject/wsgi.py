ebbcb78a6235907e2f3ad25af35dccb8b55999e9
kavindu ishara <161452109+kavinduishara@users.noreply.github.com>
2025-01-03 21:29:05 +0530

    Update wsgi.py debugging line added

--- firstProject/wsgi.py
+++ firstProject/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+print("Application starting...")
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstProject.settings')
 
 application = get_wsgi_application()
